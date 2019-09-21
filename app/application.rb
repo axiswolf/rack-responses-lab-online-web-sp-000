0f9352bcdc26ca818849244de831945ee351da84
Karen V <karenvo@usc.edu>
2019-09-21 01:40:29 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,12 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+
+    if time = 
+      resp.write "Good Morning"
+    else 
+      resp.write "Good afternoon"
+    end
+  end
+
+end
