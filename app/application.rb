847691c54285ba8252ccf686cff6203fe3535a97
Karen V <karenvo@usc.edu>
2019-09-21 01:52:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,8 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-    time = Time.now
-    time_2 = time.strftime("%H:%M:%S")
-    if time_2 < "12"
+    t = Time.now
+    if t.hour < "12"
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
