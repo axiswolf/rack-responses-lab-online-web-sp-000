df30a1b86344a994872f059eec67b0a0b379030b
Karen V <karenvo@usc.edu>
2019-09-21 01:50:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
     resp = Rack::Response.new
     time = Time.now
     time_2 = time.strftime("%H:%M:%S")
-    if time_2 < 12:00:00
+    if time_2 < "12"
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
